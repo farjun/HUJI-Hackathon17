5027b7f29ef7011a69e80bb65da944deeac26026
Jenia <genia90@gmail.com>
2017-05-19 01:02:16 +0300

    updated server

--- src/server/server.py
+++ src/server/server.py
@@ -1,21 +1,40 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template
 import json
 import sqlite3
 
-db = sqlite3.connect('simitu.db')
+db = sqlite3.connect('mysite/simitu.db')
 app = Flask(__name__)
 
 
 @app.route('/')
-def default():
-    return 'Welcome to simitu'
+def render():
+    return render_template('fb2.html')
 
 
-@app.route('/adduser', methods=['POST'])
+@app.route('/api/getuser')
+def get_user():
+    uid = request.args['id']
+    curs = db.cursor()
+    curs.execute("SELECT * FROM USERS WHERE ID=?", (uid,))
+    q = curs.fetchall()
+
+    return json.dumps({"uid": q[0],
+                       "name": q[1],
+                       "age:": q[2],
+                       "profile": q[3],
+                       "cover": [4],
+                       "event": q[5],
+                       "likes": q[6],
+                       "favorites": q[7],
+                       "interests": q[8].split()})
+
+
+@app.route('/api/adduser', methods=['POST'])
 def add_user():
     curs = db.cursor()
     j = request.json
-    id = j['id']
+    print(j)
+    uid = j['uid']
     name = j['name']
     age = j['age']
     profile_photo = j['profile']
@@ -26,66 +45,62 @@ def add_user():
 
     try:
         curs.execute("INSERT INTO USERS VALUES (?,?,?,?,?,0,0,'',?)",
-                     (id, name, age, profile_photo, cover_photo, interest[1:],))
+                     (uid, name, age, profile_photo, cover_photo,
+                      interest[1:],))
         db.commit()
     except Exception as e:
-        return e
+        print(e)
+        return 'BAD'
 
     return 'OK'
 
 
+@app.route('/api/addrequest')
 def add_request():
-    event_id = request.args["eventID"]
-    user_id = request.args["userID"]
-
+    event_id = request.args["eid"]
+    user_id = request.args["uid"]
+    print("add: " + event_id + " " + user_id)
     c = db.cursor()
-    try:
-        c.execute("INSERT INTO notifications VALUES (userID=?,"
-                  "otherUserID=?)", (user_id, event_id,))
-        c.execute("INSERT INTO notifications (userID,type, otherUserID) \
-          VALUES (" + event_id + ", 'request' ," + user_id + ",)")
-        db.commit()
-    except:
-        return "BAD"
+
+    c.execute("INSERT INTO notifications VALUES (?, ?, ?)",
+              (None, event_id, 'request', user_id,))
+    db.commit()
 
     return "OK"
 
 
+@app.route('/api/getnotification')
 def get_notifications():
+    user_id = request.args["userID"]
 
-  user_id =request.args["userID"]
-  try:
     c = db.cursor()
-    c.execute("SELECT (type, otherUserID)  FROM notifications WHERE userID='%s'" % user_id)
-  except:
-    return "BAD"
+    c.execute("SELECT type, otherUserID  FROM notifications WHERE userID=?",
+              (user_id,))
 
-  return json.dump(c.fetchall())
+    return json.dumps(c.fetchall())
 
 
+@app.route('/api/approveuser')
 def approve_user():
+    user_to_approve = request.args["approveid"]
+    user_id = request.args["uid"]
+    approve = request.args["approve"]
 
-  user_to_approve = request.args["userToApprove"]
-  user_id = request.args["userID"]
-  approve  = request.args["approve"]
-  try:
     c = db.cursor()
-    c.execute("DELETE FROM notifications WHERE type='request' and otherUserID="+user_to_approve+ \
-              " and userID="+user_id)
+    c.execute(
+        "DELETE FROM notifications WHERE type='request' AND otherUserID=" + user_to_approve + \
+        " AND userID=" + user_id)
     db.commit()
-  except:
-    return "BAD"
 
-  #if approve=true : insert into ATTENDEES
-  if approve:
-    try:
-      c.execute("INSERT INTO ATTENDEES (eventID, userID ) \
-        VALUES (" + user_id + "," + user_to_approve + " )")
-      db.commit()
-    except:
-      return "BAD"
+    # if approve=true : insert into ATTENDEES
+    if approve:
+        c.execute("INSERT INTO ATTENDEES VALUES (?, ?)",
+                  (user_id, user_to_approve,))
+        db.commit()
 
-  return "OK"
+    return "OK"
+
 
 if __name__ == '__main__':
-    app.run('0.0.0.0', 5000)
+    app.run()
+    db.close()
