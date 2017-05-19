42f3f2825cab70d33d2402f806192eced49a5dc2
farjun <omerbh1@gmail.com>
2017-05-19 07:01:26 +0300

    eee

--- backbone/app/components/controllers/AboutController.js
+++ backbone/app/components/controllers/AboutController.js
@@ -28,6 +28,15 @@ angular.module('mostPopularListingsApp.about', ['ngRoute'])
 	
 	};
 
+    function getParameterByName(name, url) {
+        if (!url) url = window.location.href;
+        name = name.replace(/[\[\]]/g, "\\$&");
+        var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+            results = regex.exec(url);
+        if (!results) return null;
+        if (!results[2]) return '';
+        return decodeURIComponent(results[2].replace(/\+/g, " "));
+    }
 
 	$scope.images=["resources/img/Event1Large.jpg","resources/img/Event2.jpg","resources/img/Event3.jpg"]
 	$scope.switchImg=function (num) {
@@ -36,6 +45,14 @@ angular.module('mostPopularListingsApp.about', ['ngRoute'])
 		$scope.images[num-1] = temp;
     };
 
+	$scope.currentEvent = getParameterByName('eid');
+	console.log("-------------",$scope.currentEvent);
+	if($scope.currentEvent==null){
+		alert("self");
+        document.getElementById("portfolio").style.display = 'none';
+	}else{
+		alert("event id " + $scope.currentEvent)
+	}
 
 	this.message = "Hello About!";
 
